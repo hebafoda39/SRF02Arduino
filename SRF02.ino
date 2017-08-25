afec06cd20cfd8d32ce0920d07ec11cb3660cf6e
Faraz Ahmed <farazbhatti117@gmail.com>
2017-08-25 23:25:12 +0200

    Add files via upload

---
+++ SRF02.ino
@@ -0,0 +1,16 @@
+#include<Wire.h> // Library for I2C its a 2 wire protocol
+
+void setup() {
+  // put your setup code here, to run once:
+ Wire.begin(); 
+ Serial.begin(115200);
+}
+int readingInCms = 0;
+int address = 112; // 0x70
+// The original address is 0xE0
+void loop() {
+      readingInCms= getDistance(address);
+      Serial.print("Object Detected at: ");
+      Serial.println(readingInCms); 
+      delay(200);
+}
---
+++ SRF02Func.ino
@@ -0,0 +1,21 @@
+int getDistance(int address){
+    // put your main code here, to run repeatedly:
+  Wire.beginTransmission(address);
+  Wire.write(byte(0x00));
+  Wire.write(byte(0x51)); // Used for telling the sensor to send back value in cm
+  Wire.endTransmission();
+  // So this here code would tell the sensor to do its work
+  delay(70); 
+  Wire.beginTransmission(address);
+  Wire.write(byte(0x02)); // Sets the register pointer
+  Wire.endTransmission();
+
+  Wire.requestFrom(112,2); // We request 2 bytes from the sensor
+  if(2 <=Wire.available()){
+      readingInCms = Wire.read();
+      readingInCms = readingInCms << 8;
+      readingInCms |= Wire.read();
+      return readingInCms;
+   }
+}
+
